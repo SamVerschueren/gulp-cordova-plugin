5f6b8257523e7a1f58b711fc6eda6cf103ace4eb
Sam Verschueren <sam.verschueren@gmail.com>
2015-06-10 19:20:52 +0200

    Added possibility to pass array of plugins

--- index.js
+++ index.js
@@ -11,10 +11,14 @@
 var path = require('path'),
     through = require('through2'),
     gutil = require('gulp-util'),
-    cordova = require('cordova-lib').cordova.raw;
+    cordova = require('cordova-lib').cordova.raw,
+    Q = require('q');
 
 // export the module
-module.exports = function(plugin) {
+module.exports = function(plugins) {
+
+    // Make sure it is an array of plugins
+    plugins = [].concat(plugins);
 
     return through.obj(function(file, enc, cb) {
         // Change the working directory
@@ -25,15 +29,26 @@ module.exports = function(plugin) {
 
         cb();
     }, function(cb) {
-        // Print which plugin will be added
-        gutil.log('\tadd ' + plugin);
-
-        // Execute the cordova plugin add command
-        cordova.plugin('add', plugin)
-            .then(cb)
+        var promises = plugins.map(add);
+        
+        Q.all(promises)
+            .then(function() {
+                // Call the callback if all the plugins are added correctly
+                cb();
+            })
             .catch(function(err) {
                 // Return an error if something happened
                 cb(new gutil.PluginError('gulp-cordova-plugin', err.message));
             });
     });
 };
+
+function add(plugin) {
+    return Q.fcall(function() {
+        // Print which plugin will be added
+        gutil.log('\tadd ' + plugin);
+        
+        // Add the plugin
+        return cordova.plugin('add', plugin);
+    });
+}
